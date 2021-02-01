5ce4aea2e93b3cd3430e60cd22b814568272a4f1
hajkeats <hajkeats@hotmail.com>
2021-02-01 23:54:48 +0000

    Fixes to docs.

--- quiz.py
+++ quiz.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     {
         "Topic 1": {
             "Question 1": "Answer 1",
-            "Questions 2: "Answer 2"
+            "Question 2": "Answer 2"
         },
         "Topic 2" : {
             ...
